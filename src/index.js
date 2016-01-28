19d7c5ca04a594e35675d02bcaa1d0f145b0f795
notewrangler <noiseartist@gmail.com>
2016-01-28 11:20:26 -0800

    took out components/app.js moved logic to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyANPrBPM8YxKOTVFvxN0YPSdC3hFXvHSxQ'
+
+export default class App extends Component {
+  constructor(props){
+		super(props);
+
+		this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('React.js');
+	}
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+
+  render() {
+    const videoSeach = _.debounce((term) => {this.videoSearch(term)}, 400);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
 
 
 ReactDOM.render(<App /> , document.querySelector('.container'));
