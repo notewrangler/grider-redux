64dcd83d8690e4967c734360a3307ef00fea51fe
notewrangler <noiseartist@gmail.com>
2016-01-27 17:04:30 -0800

    in progress, just after adding callbacks from video_list_item, video_list

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { Component } from 'react';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyANPrBPM8YxKOTVFvxN0YPSdC3hFXvHSxQ'
 
 export default class App extends Component {
+  constructor(props){
+		super(props);
+
+		this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'dog'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    });
+	}
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) } 
+          videos={this.state.videos}/>
+      </div>
     );
   }
 }
